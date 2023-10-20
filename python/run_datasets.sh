26af49de63db8ccdbea1f6acac2c2344365c63b6
Yisu Peng <yisupeng@gmail.com>
2023-10-20 16:39:35 -0400

    run_datasets.sh

---
+++ python/run_datasets.sh
@@ -0,0 +1,33 @@
+
+datasets=$(cat <<- DOC_DATASETS
+    alban
+    Alinden
+    ALott
+    CPSF
+    D1810
+    ecoli_xl
+    MS2000225
+    peplib
+    QE
+    RPA
+DOC_DATASETS
+)
+# 	unweighted_pdf_weighted_pdf_mode
+# 	unweighted_cdf_weighted_pdf_mode
+# 	unweighted_pdf_cdf_weighted_pdf_mode
+
+# skew=$(sed -ne 's/^\s*init_skewness = \([0-9]*\).*/\1/p' run.py)
+# echo ${skew}x
+# if [[ ! -z "$2" ]]; then
+# 	conf=-c $2
+# fi
+conf=$2
+for dataset in $datasets;
+do
+	CUDA_VISIBLE_DEVICES=$1 python run.py -c $conf -d $dataset -p -i -j 1 &> ../log/${conf}_${dataset}.log &
+done
+
+tail -f ../log/${conf}_*.log
+
+
+
