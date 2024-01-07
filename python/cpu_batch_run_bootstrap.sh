15ad9fefd9af7b37e8dd1b8d6fbd4ed81dff7b2e
Yisu Peng <yisupeng@gmail.com>
2024-01-06 20:07:29 -0500

    update

--- python/cpu_batch_run_bootstrap.sh
+++ python/cpu_batch_run_bootstrap.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=xlms
 #SBATCH --nodes=1
 #SBATCH --array=0-59
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=4GB
 #SBATCH --time=04:00:00
 #SBATCH --output=out/array_%A_%a.out
@@ -16,11 +16,13 @@
 ((i = SLURM_ARRAY_TASK_ID % 10))
 ((p = SLURM_ARRAY_TASK_ID / 10))
 mu_strategy=$1
+bootstrap_i=$2
 
 echo $mu_strategy
 
-# ./run_1S_dataset_i.sh $i unweighted_pdf_mode $p $mu_strategy
-./run_dataset_i.sh $i unweighted_pdf_mode $p $mu_strategy
+# ./run_1S_dataset_i_bootstrap_i.sh $i unweighted_pdf_mode $p $mu_strategy $bootstrap_i
+# ./run_dataset_i_bootstrap_i.sh $i no_constraint $p $mu_strategy $bootstrap_i
+./run_dataset_i_bootstrap_i.sh $i unweighted_pdf_mode $p $mu_strategy $bootstrap_i
 
 
 
---
+++ python/cpu_batch_run_dataset.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=xlms_datasets
+#SBATCH --nodes=1
+#SBATCH --array=0-5
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=4GB
+#SBATCH --time=04:00:00
+#SBATCH --output=out/array_%A_%a.out
+#SBATCH --error=err/array_%A_%a.err
+
+
+# srun ./
+# echo $SLURM_ARRAY_TASK_ID
+. ~/.bash_profile
+
+# ((i = SLURM_ARRAY_TASK_ID % 10))
+# ((p = SLURM_ARRAY_TASK_ID / 10))
+i=$1
+p=$SLURM_ARRAY_TASK_ID
+
+# ./run_1S_dataset_i.sh $i unweighted_pdf_mode $p
+./run_dataset_i.sh $i unweighted_pdf_mode $p gaussian
+
+
+
